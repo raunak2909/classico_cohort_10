ee7c977e7f941afe1745400293ac3217fa06b2e3
wscubegoodtogo-beep <wscube.goodtogo@gmail.com>
2026-05-13 23:29:08 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/classico_cohort_10/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.classico_cohort_10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/practice.dart
@@ -0,0 +1,37 @@
+void main(){  ///M != m
+
+
+  //int userName = 89;
+  /// avoid these
+  /// starting with number
+  /// special char
+  /// space -> _
+  /// SnackCase -> class identifier
+  /// camelCase while giving variable and functions names
+
+
+  int sum1 = add(5,6);
+  print(sum1);
+  print(sum1*2);
+
+
+  print(add(100,400));
+
+
+
+  print(add(53,46));
+
+
+  print(add(11,21));
+
+
+}
+
+/// this function add the numbers
+int add(int no1, int no2){
+  int sum = no1+no2;
+  return sum;
+}
+
+///1. get input from console
+///2. make sub, multi, division and remainder function (+, -, *, /, %)
